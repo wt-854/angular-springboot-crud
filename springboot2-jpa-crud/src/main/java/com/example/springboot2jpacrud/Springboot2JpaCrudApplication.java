6a45cc01cc38bacb1421eb691154fbf1d99dd78a
wt-854 <wt854@uowmail.edu.au>
2020-02-28 23:47:56 +0800

    First commit

---
+++ springboot2-jpa-crud/src/main/java/com/example/springboot2jpacrud/Springboot2JpaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot2jpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2JpaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2JpaCrudApplication.class, args);
+	}
+
+}
---
+++ springboot2-jpa-crud/src/test/java/com/example/springboot2jpacrud/Springboot2JpaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot2jpacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2JpaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
